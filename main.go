22d1d2d11f68f055d0a1bc96ba9d7e02d7e56df1
azhaxylyk <zhaxylykova.a7@gmail.com>
2024-01-10 12:58:49 +0600

    start

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	fmt.Println("File opened successfully. Now reading file contents line by line:")
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
