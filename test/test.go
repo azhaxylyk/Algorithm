86032ecb1356b0ba68512b44f1c6db58a80efe9c
azhaxylyk <zhaxylykova.a7@gmail.com>
2024-01-11 11:30:25 +0600

    testing

---
+++ test/test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"sort"
+
+	"gonum.org/v1/gonum/stat"
+)
+
+func main() {
+	f, err := os.Open("test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	var xs []float64
+	scan := bufio.NewScanner(f)
+	for scan.Scan() {
+		var v float64
+		txt := scan.Text()
+		_, err = fmt.Sscanf(txt, "%f", &v)
+		if err != nil {
+			log.Fatalf(
+				"could not convert to float64 %q: %v",
+				txt, err,
+			)
+		}
+		xs = append(xs, v)
+	}
+
+	if err = scan.Err(); err != nil {
+		log.Fatalf("error scanning file: %v", err)
+	}
+
+	fmt.Printf("data sample size: %v\n", len(xs))
+
+	mean := stat.Mean(xs, nil)
+	variance := stat.Variance(xs, nil)
+	stddev := math.Sqrt(variance)
+
+	sort.Float64s(xs)
+	median := stat.Quantile(0.5, stat.Empirical, xs, nil)
+
+	fmt.Printf("mean=     %v\n", mean)
+	fmt.Printf("median=   %v\n", median)
+	fmt.Printf("variance= %v\n", variance)
+	fmt.Printf("std-dev=  %v\n", stddev)
+}
